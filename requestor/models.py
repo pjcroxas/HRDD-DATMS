83a830f199e81eaa67980e7f0f6644971993522d
PatrickJoseRoxas <patrickjose.roxas@gmail.com>
2021-06-09 08:56:19 +0800

    disable scholarship

--- requestor/models.py
+++ requestor/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Category(models.Model):
     categories = models.CharField(max_length=100)
     shortterm = models.CharField(max_length=20, null=True, blank=True)
@@ -18,8 +19,10 @@ class Category(models.Model):
     def get_short(self):
         return self.shortterm
 
+
 class SectorForScholarship(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True,  related_name="sector_for_scholarship_set")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True,  related_name="sector_for_scholarship_set")
     sectorforscholarship = models.CharField(max_length=100)
     shortterm = models.CharField(max_length=10, null=True)
 
@@ -29,6 +32,7 @@ class SectorForScholarship(models.Model):
     def natural_key(self):
         return (self.sectorforscholarship)
 
+
 class SubCategory(models.Model):
     subcategories = models.CharField(max_length=100)
 
@@ -37,6 +41,7 @@ class SubCategory(models.Model):
 
     def natural_key(self):
         return (self.subcategories)
+
 
 class SupportCategory(models.Model):
     ShortTerm = models.CharField(max_length=10, null=True)
@@ -51,6 +56,7 @@ class SupportCategory(models.Model):
     def natural_key(self):
         return (self.supportcategories)
 
+
 class Sector(models.Model):
     sector = models.CharField(max_length=100)
 
@@ -61,7 +67,6 @@ class Sector(models.Model):
         return (self.sector)
 
 
-
 class Status(models.Model):
     status = models.CharField(max_length=100)
 
@@ -69,8 +74,7 @@ class Status(models.Model):
         return self.status
 
     def natural_key(self):
-        return (self.status) 
-
+        return (self.status)
 
 
 class Requestor(models.Model):
@@ -81,22 +85,22 @@ class Requestor(models.Model):
     incrementor = models.IntegerField(default=0)
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, blank=True, null=True,
-         on_delete=models.CASCADE, default='')
+                             on_delete=models.CASCADE, default='')
     reference_scholarship = models.CharField(max_length=500, verbose_name=(
         'Reference'), default='Auto Assign', blank=True)
     ##### when i used date_filed in reference, the output is "nonetype" #####
     date_filed = models.DateTimeField(auto_now_add=True, null=True,
-         verbose_name='date_filed')
+                                      verbose_name='date_filed')
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
-         null=True, verbose_name="Category")
+                                 null=True, verbose_name="Category")
     subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE,
-         null=True, verbose_name="Sub Category")
+                                    null=True, verbose_name="Sub Category")
     supportcategory = models.ForeignKey(SupportCategory,
-         on_delete=models.CASCADE, null=True, verbose_name="Support Category")
+                                        on_delete=models.CASCADE, null=True, verbose_name="Support Category")
     sector = models.ForeignKey(Sector, on_delete=models.CASCADE,
-         null=True, verbose_name="Sector")
+                               null=True, verbose_name="Sector")
     sector_for_scholarship = models.ForeignKey(SectorForScholarship,
-         on_delete=models.CASCADE, null=True, verbose_name="Sector")
+                                               on_delete=models.CASCADE, null=True, verbose_name="Sector")
     location = models.CharField(max_length=50, blank=True, null=True)
     prog_title = models.CharField(max_length=50)
     status = models.ForeignKey(
@@ -104,25 +108,29 @@ class Requestor(models.Model):
     attachments = models.FileField(
         upload_to='uploads/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
     assigned = models.ForeignKey(User, blank=True, null=True,
-         on_delete=models.CASCADE, related_name='assigned')
+                                 on_delete=models.CASCADE, related_name='assigned')
    # field for cancel function
     reason = models.CharField(max_length=100, blank=True, null=True)
     lacking_documents = models.CharField(max_length=250, blank=True, null=True)
-    ta_signed_upload = models.FileField(max_length=100, blank=True, null = True, upload_to ='uploads/')
+    ta_signed_upload = models.FileField(
+        max_length=100, blank=True, null=True, upload_to='uploads/')
     taketimestamp = models.DateTimeField(blank=True, null=True)
+
+    sa_signed_upload = models.FileField(
+        upload_to='sa_signed_upload/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
 
     # filing_date = models.DateField(null=True) ## Temporary
     start_date = models.DateField(null=True)
     end_date = models.DateField(null=True)
-    req_action = models.CharField(max_length=50, default = 0)
+    req_action = models.CharField(max_length=50, default=0)
     gender = models.CharField(max_length=100, blank=True, null=True)
     scho_donor = models.CharField(max_length=100, blank=True, null=True)
     other_expense = models.CharField(max_length=100, blank=True, null=True)
-    service_obligation =  models.CharField(max_length=100, blank=True, null=True)
+    service_obligation = models.CharField(
+        max_length=100, blank=True, null=True)
     echo = models.CharField(max_length=100, blank=True, null=True)
     training_report = models.CharField(max_length=100, blank=True, null=True)
     remarks = models.CharField(max_length=100, blank=True, null=True)
-
 
     # Nomination forms
     name_title = models.CharField(max_length=100, default='')
@@ -138,7 +146,7 @@ class Requestor(models.Model):
     office_no = models.CharField(max_length=100, default='')
     address = models.CharField(max_length=100, default='')
 
-    ## Gedsi
+    # Gedsi
     PENDING_CHOICES = (
         ('Male', 'Male'),
         ('Female', 'Female')
@@ -150,28 +158,31 @@ class Requestor(models.Model):
         ('annulled', 'Annulled'),
         ('widowed', 'Widowed')
     )
-    gender = models.CharField(max_length=10, choices=PENDING_CHOICES, default='')
+    gender = models.CharField(
+        max_length=10, choices=PENDING_CHOICES, default='')
     age = models.IntegerField(null=True, blank=True)
-    civil_status = models.CharField(max_length=20, choices=CIVIL_STATUS_CHOICES, default='')
+    civil_status = models.CharField(
+        max_length=20, choices=CIVIL_STATUS_CHOICES, default='')
     disability = models.CharField(max_length=100, null=True, blank=True)
     ethnicity = models.CharField(max_length=100, null=True, blank=True)
-    duties = models.CharField(max_length= 400, null=True, blank=True)
+    duties = models.CharField(max_length=400, null=True, blank=True)
 
-    ## Eligibility Status
+    # Eligibility Status
     PENDING_CHOICES = (
         ('True', 'Yes'),
         ('False', 'No'),
     )
-    pending_scholarship = models.CharField(max_length=10, choices=PENDING_CHOICES, null=True)
+    pending_scholarship = models.CharField(
+        max_length=10, choices=PENDING_CHOICES, null=True)
     scholarship_title = models.CharField(max_length=100, null=True)
-    pending_reentry_action_plan = models.CharField(max_length=10, choices=PENDING_CHOICES, null=True, blank=True)
+    pending_reentry_action_plan = models.CharField(
+        max_length=10, choices=PENDING_CHOICES, null=True, blank=True)
     reap_title = models.CharField(max_length=100, null=True, blank=True)
 
     last_file = models.FileField(
         upload_to='signed_nomination/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
 
-
-    ## Preselection
+    # Preselection
     pending_task = models.FileField(
         upload_to='pending_tasks/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
     service_record = models.FileField(
@@ -182,27 +193,29 @@ class Requestor(models.Model):
     attended_training = models.FileField(
         upload_to='a_t/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
 
-
-    ## Scholarship Requirements
+    # Scholarship Requirements
     personal_data_sheet = models.FileField(
         upload_to='pds/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
-    individual_performance_commitment_and_review =  models.FileField(
+    individual_performance_commitment_and_review = models.FileField(
         upload_to='ipcr/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
-    potential_assessment_form =  models.FileField(
+    potential_assessment_form = models.FileField(
         upload_to='paf/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
     justification = models.FileField(
         upload_to='justification/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
 
     ## Terms & Conditions
-    checkbox_certify = models.BooleanField(verbose_name="Certify", default="False")
-    checkbox_ack = models.BooleanField(verbose_name="Acknowledgement", default='False')
+    checkbox_certify = models.BooleanField(
+        verbose_name="Certify", default="False")
+    checkbox_ack = models.BooleanField(
+        verbose_name="Acknowledgement", default='False')
     checkbox_agree = models.BooleanField(verbose_name="Agree", default='False')
 
     FOLLOWCHOICE = (
-    (1, 'True'),
-    (0, 'False'),
+        (1, 'True'),
+        (0, 'False'),
     )
-    followup = models.BooleanField(verbose_name="Follow up to HRDD", choices = FOLLOWCHOICE, default="False")
+    followup = models.BooleanField(
+        verbose_name="Follow up to HRDD", choices=FOLLOWCHOICE, default="False")
 
     @property
     def filename(self):
@@ -218,7 +231,8 @@ class Requestor(models.Model):
 
     @property
     def extension(self):
-        name, extension = os.path.splitext(self.individual_performance_commitment_and_review.name)
+        name, extension = os.path.splitext(
+            self.individual_performance_commitment_and_review.name)
 
     @property
     def filename(self):
@@ -228,9 +242,6 @@ class Requestor(models.Model):
     def extension(self):
         name, extension = os.path.splitext(self.potential_assessment_form.name)
 
-
-
-
     @property
     def filename(self):
         return os.path.basename(self.attachments.name)
@@ -238,7 +249,6 @@ class Requestor(models.Model):
     @property
     def extension(self):
         name, extension = os.path.splitext(self.attachments.name)
-
 
     def __str__(self):
         return self.reference
@@ -252,23 +262,26 @@ class Requestor(models.Model):
     def save(self):
         if self.incrementor == 0:
             if self.category.id == 1:
-                latest_incrementor = Requestor.objects.all().filter(category=1).values('incrementor').last()
+                latest_incrementor = Requestor.objects.all().filter(
+                    category=1).values('incrementor').last()
                 latest_incrementor = latest_incrementor['incrementor']
                 self.incrementor = latest_incrementor + 1
-                ## Uncomment this for first request
+                # Uncomment this for first request
                 #self.incrementor = self.incrementor + 0
                 super(Requestor, self).save()
             elif self.category.id == 2:
-                latest_incrementor = Requestor.objects.all().filter(category=2).values('incrementor').last()
+                latest_incrementor = Requestor.objects.all().filter(
+                    category=2).values('incrementor').last()
                 latest_incrementor = latest_incrementor['incrementor']
                 self.incrementor = latest_incrementor + 1
-                ## Uncomment this for first request
+                # Uncomment this for first request
                 #self.incrementor = self.incrementor + 0
                 super(Requestor, self).save()
             else:
                 super(Requestor, self).save()
         else:
             super(Requestor, self).save()
+
 
 class Nomination_Form(models.Model):
     nomination_id = models.AutoField(primary_key=True)
@@ -288,7 +301,7 @@ class Nomination_Form(models.Model):
     office_no = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
 
-    ## Gedsi
+    # Gedsi
     PENDING_CHOICES = (
         ('Male', 'Male'),
         ('Female', 'Female')
@@ -300,27 +313,32 @@ class Nomination_Form(models.Model):
         ('annulled', 'Annulled'),
         ('widowed', 'Widowed')
     )
-    gender = models.CharField(max_length=10, choices=PENDING_CHOICES, default='')
+    gender = models.CharField(
+        max_length=10, choices=PENDING_CHOICES, default='')
     age = models.IntegerField(null=True, blank=True)
-    civil_status = models.CharField(max_length=20, choices=CIVIL_STATUS_CHOICES, default='')
+    civil_status = models.CharField(
+        max_length=20, choices=CIVIL_STATUS_CHOICES, default='')
     disability = models.CharField(max_length=100, null=True, blank=True)
     ethnicity = models.CharField(max_length=100, null=True, blank=True)
-    duties = models.CharField(max_length= 400, null=True, blank=True)
+    duties = models.CharField(max_length=400, null=True, blank=True)
 
     def __str__(self):
         return str(self.requestor)
 
 
 class Eligibility_Status(models.Model):
-    requestor = requestor = models.OneToOneField(Requestor, on_delete=models.CASCADE)
+    requestor = requestor = models.OneToOneField(
+        Requestor, on_delete=models.CASCADE)
     eligibility_id = models.AutoField(primary_key=True)
     PENDING_CHOICES = (
         ('True', 'Yes'),
         ('False', 'No'),
     )
-    pending_scholarship = models.CharField(max_length=10, choices=PENDING_CHOICES)
+    pending_scholarship = models.CharField(
+        max_length=10, choices=PENDING_CHOICES)
     scholarship_title = models.CharField(max_length=100)
-    pending_reentry_action_plan = models.CharField(max_length=10, choices=PENDING_CHOICES)
+    pending_reentry_action_plan = models.CharField(
+        max_length=10, choices=PENDING_CHOICES)
     reap_title = models.CharField(max_length=100)
 
     def __str__(self):
@@ -331,9 +349,9 @@ class Scholarship_Requirements(models.Model):
     requestor = models.OneToOneField(Requestor, on_delete=models.CASCADE)
     personal_data_sheet = models.FileField(
         upload_to='pds/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
-    individual_performance_commitment_and_review =  models.FileField(
+    individual_performance_commitment_and_review = models.FileField(
         upload_to='ipcr/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
-    potential_assessment_form =  models.FileField(
+    potential_assessment_form = models.FileField(
         upload_to='paf/%Y/%m/%d/', validators=[validate_file_extension], null=True, blank=True)
 
     @property
@@ -350,7 +368,8 @@ class Scholarship_Requirements(models.Model):
 
     @property
     def extension(self):
-        name, extension = os.path.splitext(self.individual_performance_commitment_and_review.name)
+        name, extension = os.path.splitext(
+            self.individual_performance_commitment_and_review.name)
 
     @property
     def filename(self):
@@ -375,21 +394,19 @@ class TermsCondition(models.Model):
     requestor = models.OneToOneField(Requestor, on_delete=models.CASCADE)
     checkbox_certify = models.BooleanField(verbose_name=label_certify)
     checkbox_ack = models.BooleanField(verbose_name=label_ack, default='False')
-    checkbox_agree = models.BooleanField(verbose_name=label_agree, default='False')
+    checkbox_agree = models.BooleanField(
+        verbose_name=label_agree, default='False')
 
     def __str__(self):
         return str(self.requestor)
 
 
-
-
-
 class RequestorLog(models.Model):
     log_id = models.CharField(max_length=100, primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,
-         null=True, verbose_name="Users")
+                             null=True, verbose_name="Users")
     content_type = models.ForeignKey(Status, on_delete=models.CASCADE,
-         null=True, verbose_name="content")
+                                     null=True, verbose_name="content")
 
     change_msg = models.CharField(max_length=100)
 
